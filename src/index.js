80c66eedc4171e770b4fba09b8c95835dfc14983
Roshel Esteron <mynameisroshel19@gmail.com>
2022-10-12 22:03:23 +1000

    commit tree e667a2c850efdeeb6d81991d1cca725b8308d5ab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        checkMe = event.target.children[1].value;
+        console.log(checkMe)
+        const input = document.querySelector('input#searchByID');
+        console.log('This is from doc');
+        console.log(input.value); 
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const title = document.querySelector('h4');
+            title.innerText = data.title;
+            const summary = document.querySelector('p');
+            summary.innerText = data.summary;
+            
+        });    
+});
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
